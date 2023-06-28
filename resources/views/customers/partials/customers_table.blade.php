e50f0d543419a5ce0cec1d0439a36f2be194514d
pravinpimpale <pravinpimpale45@gmail.com>
2023-06-28 16:09:41 +0530

    #157 Customers > removed gap bwtn two Customer

--- resources/views/customers/partials/customers_table.blade.php
+++ resources/views/customers/partials/customers_table.blade.php
@@ -5,8 +5,8 @@
                 @foreach ($customers as $customer)
                     <a href="{{ Eventy::filter('customer.card.url', route('customers.update', ['id' => $customer->id]), $customer) }}" class="card hover-shade  col-lg-3 col-md-3 col-sm-5 col-12" @action('customer.card.link', $customer) >
                         <img src="{{ $customer->getPhotoUrl() }}" />
-                        <h4>{{ $customer->first_name }} {{ $customer->last_name }}</h4>
-                        <p class="text-truncate"><small>{{ $customer->getEmailOrPhone() }}</small></p>
+                        <h4>{{ $customer->first_name? $customer->first_name : '-' }} {{ $customer->last_name}}</h4>
+                        <p class="text-truncate"><small>{{ $customer->getEmailOrPhone()? $customer->getEmailOrPhone() : '-' }}</small></p>
                     </a>
                 @endforeach
             </div>
