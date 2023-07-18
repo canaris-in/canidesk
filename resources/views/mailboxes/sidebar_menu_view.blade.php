5c94199a13a2a2653c0ec02d058ca538b656206e
pravinpimpale <pravinpimpale45@gmail.com>
2023-07-18 10:02:43 +0530

    add php line on head section

--- resources/views/mailboxes/sidebar_menu_view.blade.php
+++ resources/views/mailboxes/sidebar_menu_view.blade.php
@@ -1,3 +1,8 @@
+<?php
+/** @var \App\Folder $folder */
+?>
+<div class="alert alert-danger errormsg glyphicon glyphicon-exclamation-sign" id="errorMsg" style="display: none;">
+</div>
 <div class="dropdown sidebar-title sidebar-title-extra">
     <span class="sidebar-title-extra-value active-count">{{ $folder->getTypeName() }}
         ({{ $folder->active_count }})</span>
